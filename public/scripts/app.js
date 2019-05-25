65add0cc616902b87fe311b76a62e4f9d0f8ea44
Olivia Ester <oliviaesterdev@gmail.com>
2019-05-25 14:14:55 -0700

    es6-classes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,45 @@
 'use strict';
 
-var visibility = false;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var render = function render() {
-    var jsx = React.createElement(
-        'div',
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Header = function (_React$Component) {
+    _inherits(Header, _React$Component);
+
+    function Header() {
+        _classCallCheck(this, Header);
+
+        return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+    }
+
+    _createClass(Header, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'p',
+                null,
+                'This is from Header'
+            );
+        }
+    }]);
+
+    return Header;
+}(React.Component);
+
+var jsx = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
         null,
-        React.createElement(
-            'h1',
-            null,
-            'Visible Toggle'
-        )
-    );
+        'Title'
+    ),
+    React.createElement(Header, null)
+);
 
-    ReactDOM.render(jsx, document.getElementById('app'));
-};
-
-render();
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,9 +1,22 @@
 let visibility = false;
 
+const toggleVisivility = () => {
+    visibility = !visibility;
+    render();
+}
+
 const render = () => {
     const jsx = (
         <div>
             <h1>Visible Toggle</h1>
+            <button onClick={toggleVisivility}>
+            {visibility ? 'Hide details': 'Show details'}
+            </button>
+            {visibility && (
+                <div>
+                <p>These are some details</p>
+                </div>
+            )}
         </div>
     );
 

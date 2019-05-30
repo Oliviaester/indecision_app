892e9a5f4fc53d2754f980b3af5b14c5ae42ee40
Olivia Ester <oliviaesterdev@gmail.com>
2019-05-30 16:24:50 -0700

    local storage

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count
+            count: 0
         }
     }
     handleAddOne() {
@@ -40,10 +40,6 @@ class Counter extends React.Component {
         );
     }
 }
-
-Counter.defaultProps = {
-    count: 0
-};
 
 ReactDOM.render(<Counter />, document.getElementById('app'))
 

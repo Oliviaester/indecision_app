5af61fbb5c27a558a3fc9c656543359fee957a8d
Olivia Ester <oliviaesterdev@gmail.com>
2019-06-01 15:51:40 -0700

    webpack

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-// import './utils.js';
-// import substract, {square, add} from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log("app.js is running");
-
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(substract(100, 23));
-
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(20));
-console.log(canDrink(15));
-console.log(isSenior(45));
+const template = <p>this is jsx</p>
+ReactDOM.render(template, document.getElementById('app'));
